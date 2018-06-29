55e1f8705c067ad648afa88a0af3b8bc1aff4217
pinkywhale <erica_eli@hotmail.com.ar>
2018-06-29 12:36:36 -0300

    sigo sin poder recuperar los datos del body

--- Modulo5/TPN5/tp5/routes/index.js
+++ Modulo5/TPN5/tp5/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var controlador = require('../controller/control');
+var controlador = require('../controller/control.js');
 
 //GET
 router.get('/', controlador.renIndex);
